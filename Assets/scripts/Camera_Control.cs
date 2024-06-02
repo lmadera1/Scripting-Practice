547c609fbade074bcd13365f156890fa683ff1c4
lmadera1 <luismmadera.lm@gmail.com>
2024-06-02 14:45:41 -0700

    Added script for mouse camera rotation.

--- Assets/scripts/Camera_Control.cs
+++ Assets/scripts/Camera_Control.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Camera_Control : MonoBehaviour
 {
+    float pitch = 0.0f;
+    float yaw = 0.0f;
+    float xSpeed = 3f;
+    float ySpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class Camera_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        yaw += xSpeed * Input.GetAxisRaw("Mouse X");
+        pitch -= ySpeed * Input.GetAxisRaw("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
